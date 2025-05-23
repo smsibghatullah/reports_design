e38af5997193b6c0568bd482c7d77fe992f76938
mubeen1020 <m.mubeen1020@gmail.com>
2025-05-23 16:44:28 +0500

    working

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,34 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Reports Design',
-    'version': '1.0',
-    'summary': 'Custom PDF Report Layouts',
-    'category': 'Tools',
-    'author': 'DSM',
-    'depends': ['base', 'web', 'sale','bi_sale_purchase_discount_with_tax'],  # Add 'account' if invoice reports
+    'name': "reports_design",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "DSM",
+    'website': "https://www.dsmpk.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
     'data': [
-        'report/templates.xml',
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
     ],
-    'installable': True,
-    'application': False,
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
 }
+
