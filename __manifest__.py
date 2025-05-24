f8e69f4f4cf1c0f84d4ec579bc4fe6db2294356f
mubeen1020 <m.mubeen1020@gmail.com>
2025-05-24 11:04:53 +0500

    working

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','bi_sale_purchase_discount_with_tax'],
+    'depends': ['base','sale','bi_sale_purchase_discount_with_tax','sh_amount_in_words'],
 
     # always loaded
     'data': [
